4c4563b5393e40283a7b4831ce6f7dbbfe684311
Deniz <>
2023-10-25 14:28:26 +0300

    added back buton item

--- TravioApp/TravioApp/TravioApp/Views/SignUpVC.swift
+++ TravioApp/TravioApp/TravioApp/Views/SignUpVC.swift
@@ -8,13 +8,13 @@
 import UIKit
 
 class SignUpVC: UIViewController {
-
     
     
-
+    
+    
     private lazy var contentViewBig: UIView = {
         let view = UIView()
-        view.backgroundColor = UIColor(named: "LoginBigViewColor")
+        view.backgroundColor = UIColor(named: "viewBackgroundColor")
         view.clipsToBounds = true
         view.layer.cornerRadius = 80
         view.layer.maskedCorners = [.layerMinXMinYCorner]
@@ -23,46 +23,59 @@ class SignUpVC: UIViewController {
     
     private lazy var stackViewMain: UIStackView = {
         let stackViews = UIStackView()
-        stackViews.backgroundColor = UIColor(named: "LoginBigViewColor")
+        stackViews.backgroundColor = UIColor(named: "viewBackgroundColor")
         stackViews.axis = .vertical
         stackViews.spacing = 24
         return stackViews
     }()
     private lazy var signUpButton: UIButton = {
-        let signUpButton = UIButton()
+        let signUpButton = AppButton()
         signUpButton.setTitle("Sign Up", for: .normal)
         signUpButton.titleLabel?.font = UIFont(name: "Poppins-Regular", size: 16)
-        signUpButton.backgroundColor = UIColor(named: "BackgroundColor")
+        signUpButton.backgroundColor = UIColor(named: "backgroundColor")
         signUpButton.layer.cornerRadius = 12
         return signUpButton
     }()
+    private lazy var leftBarButton: UIBarButtonItem = {
+        let leftBarButton = UIBarButtonItem()
+        leftBarButton.tintColor = .white
+        leftBarButton.image = UIImage(named: "leftArrow")
+        leftBarButton.target = self
+        leftBarButton.action = #selector(backButtonTapped)
+        return leftBarButton
+    }()
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-setupViews()
+        setupViews()
         // Do any additional setup after loading the view.
+    }
+    
+    @objc func backButtonTapped(){
+        self.navigationController?.popViewController(animated: true)
     }
     
     func setupViews() {
         
-       // imageView.frame = CGRect(x: 120, y: 64, width:149, height: 178)
-        
-        self.view.backgroundColor = UIColor(named: "BackgroundColor")
-        self.view.addSubview(contentViewBig)
-      
+        // imageView.frame = CGRect(x: 120, y: 64, width:149, height: 178)
+        self.view.backgroundColor = UIColor(named: "backgroundColor")
+        self.view.addSubviews(contentViewBig)
+        self.navigationItem.leftBarButtonItem = leftBarButton
+
         contentViewBig.addSubview(stackViewMain)
         contentViewBig.addSubview(signUpButton)
-      
-
+        
+        
         stackViewMain.addArrangedSubviews(AppTextField(data: .username), AppTextField(data: .email), AppTextField(data: .password), AppTextField(data: .passwordConfirm))
-
-       
-
+        
+        
+        
         setupLayout()
     }
-
+    
     func setupLayout() {
         let limits = self.view.safeAreaLayoutGuide.snp
-
+        
         
         contentViewBig.snp.makeConstraints { view in
             view.height.equalToSuperview().multipliedBy(0.8)
@@ -70,7 +83,7 @@ setupViews()
             view.trailing.equalToSuperview()
             view.bottom.equalToSuperview()
         }
-   
+        
         stackViewMain.snp.makeConstraints { stack in
             stack.leading.equalToSuperview().offset(24)
             stack.trailing.equalToSuperview().offset(-24)
@@ -81,11 +94,11 @@ setupViews()
             btn.trailing.equalToSuperview().offset(-24)
             btn.leading.equalToSuperview().offset(24)
             btn.height.equalTo(54)
-
+            
         })
         
-   
+        
     }
     
-
+    
 }
