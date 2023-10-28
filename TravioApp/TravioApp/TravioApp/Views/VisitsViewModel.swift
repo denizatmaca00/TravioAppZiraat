b3e3b8329c6db13264cb8e0dfb6eb3132b61ffc1
Aydın Erol <>
2023-10-28 15:11:19 +0300

    Appointed dummy data to favorites to test to test tableView and viewCell functionality

--- TravioApp/TravioApp/TravioApp/Views/VisitsViewModel.swift
+++ TravioApp/TravioApp/TravioApp/Views/VisitsViewModel.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class VisitsViewModel{
     
-    private var favorites: [Visit] = [Visit]()
+    // dummy data for tableView
+    private var favorites: [Place] = [Place(id: "1", creator: "Avni", place: "Colloseo", title: "KolezyumBaşlık", description: "Kolezyuma gittim geldim falan", cover_image_url: "https://myimage.com/colosseum", latitude: 27.232323, longitude: 15.35215, created_at: "2023-10-28", updated_at: "2023-10-28"),
+                                      Place(id: "1", creator: "Mehmet", place: "Ayasofya", title: "AyasofyaBaşlık", description: "Ayasofya'da 2 rekat kıldım gittim geldim falan", cover_image_url: "https://myimage.com/hagiasophia", latitude: 23.232323, longitude: 17.35215, created_at: "2023-10-28", updated_at: "2023-10-28")]
     
     private var cellViewModels: [VisitCellViewModel] = [VisitCellViewModel]() {
         didSet {
@@ -18,27 +20,28 @@ class VisitsViewModel{
         }
     }
     
-    
     var numberOfCells:Int {
-        return favorites.count
+        return cellViewModels.count
     }
     
     // this will be filled on VisitsVC to populate tableView with updated data
     var reloadTableViewClosure: (()->())?
     
     func initFetch(){
-        
+        // here places will be fetch from the server using .visits for VisitsVC and will be used to fill favorites:[Place/Visit] array
         //let postParams = ["id": 1]
-        NetworkingHelper.shared.dataFromRemote(urlRequest: .visits) { [weak self] (result:Result<Place, Error>) in
-            print(result)
-            self?.fetchVisits(favorites: self?.favorites ?? [])
-        }
+//        NetworkingHelper.shared.dataFromRemote(urlRequest: .visits) { [weak self] (result:Result<Place, Error>) in
+//            print(result)
+//            self?.fetchVisits(favorites: self?.favorites ?? [])
+//        }
+        fetchVisits(favorites: favorites)
     }
     
     // MARK: Create View Model
     
-    func fetchVisits(favorites:[Visit]){
+    private func fetchVisits(favorites:[Place]){
         self.favorites = favorites
+        
         var viewModels = [VisitCellViewModel]()
         
         for favorite in favorites {
@@ -47,12 +50,13 @@ class VisitsViewModel{
         self.cellViewModels = viewModels
     }
     
-    func createCellViewModel(favorite:Visit) -> VisitCellViewModel{
+    private func createCellViewModel(favorite:Place) -> VisitCellViewModel{
+        // converts info contained in "MyVisits" and adapts to CellViewModel for each VisitCell
         
         // here favorite.place_id 's are placeholders, a function should take place_id and convert into placeName and placeCity.
-        let cvm = VisitCellViewModel(placeName: favorite.place_id,
-                                     city: favorite.place_id)
-        
+        let cvm = VisitCellViewModel(image: UIImage(named: "deneme")!, 
+                                     placeName: favorite.place,
+                                     city: favorite.creator)
         return cvm
     }
     
@@ -63,6 +67,7 @@ class VisitsViewModel{
 }
 
 struct VisitCellViewModel{
+    let image:UIImage
     let placeName:String
     let city:String
 }
