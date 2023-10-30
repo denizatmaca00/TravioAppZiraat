993695fa95c3b3c010004f957e393a6f37f4ecbe
Deniz <>
2023-10-30 13:49:46 +0300

    added KeychainHelper class

---
+++ TravioApp/TravioApp/TravioApp/KeychainHelper.swift
@@ -0,0 +1,101 @@
+//
+//  KeychainHelper.swift
+//  TravioApp
+//
+//  Created by web3406 on 10/30/23.
+//
+import Foundation
+
+
+final class KeychainHelper {
+    
+    static let shared = KeychainHelper()
+    
+    init(){ }
+    
+    func save(_ data:Data, service:String, account:String) {
+        
+        let query = [
+            kSecValueData: data,
+            kSecAttrService: service,
+            kSecAttrAccount: account,
+            kSecClass: kSecClassGenericPassword
+        ] as CFDictionary
+        
+        
+        let status = SecItemAdd(query, nil)
+        
+        if status == errSecDuplicateItem {
+            
+            let query = [
+                kSecAttrService: service,
+                kSecAttrAccount: account,
+                kSecClass: kSecClassGenericPassword,
+            ] as CFDictionary
+            
+            let attributesToUpdate = [kSecValueData: data] as CFDictionary
+            
+            SecItemUpdate(query, attributesToUpdate)
+        }
+    }
+    
+    func read(service:String,account:String)->Data? {
+        
+        let query = [
+            kSecAttrService: service,
+            kSecAttrAccount: account,
+            kSecClass: kSecClassGenericPassword,
+            kSecReturnData: true
+        ] as CFDictionary
+        
+        var result:AnyObject?
+        SecItemCopyMatching(query, &result)
+        
+        
+        return (result as? Data)
+    }
+    
+    
+    func delete(_ service:String, account:String){
+        
+        let query = [
+            kSecAttrService: service,
+            kSecAttrAccount: account,
+            kSecClass: kSecClassGenericPassword
+        ] as CFDictionary
+        
+        SecItemDelete(query)
+    }
+ 
+    func getAllKeyChainItemsOfClass(_ secClass: String) -> [String:String] {
+
+        let query: [String: Any] = [
+            kSecClass as String : secClass,
+            kSecReturnData as String  : kCFBooleanTrue,
+            kSecReturnAttributes as String : kCFBooleanTrue,
+            kSecReturnRef as String : kCFBooleanTrue,
+            kSecMatchLimit as String : kSecMatchLimitAll
+        ]
+
+        var result: AnyObject?
+
+        let lastResultCode = withUnsafeMutablePointer(to: &result) {
+            SecItemCopyMatching(query as CFDictionary, UnsafeMutablePointer($0))
+        }
+
+        var values = [String:String]()
+        if lastResultCode == noErr {
+            let array = result as? Array<Dictionary<String, Any>>
+
+            for item in array! {
+                if let key = item[kSecAttrAccount as String] as? String,
+                    let value = item[kSecValueData as String] as? Data {
+                    values[key] = String(data: value, encoding:.utf8)
+                }
+            }
+        }
+
+        return values
+    }
+    
+}
--- TravioApp/TravioApp/TravioApp/SceneDelegate.swift
+++ TravioApp/TravioApp/TravioApp/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-        let vc = MapVC()
+        let vc = LoginViewController()
         let rootViewController = UINavigationController(rootViewController: vc)
         window.rootViewController = rootViewController
         window.makeKeyAndVisible()
