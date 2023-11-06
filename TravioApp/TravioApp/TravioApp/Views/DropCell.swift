7f182b3b33a4276ec5c4c8893dcd27b1aa3b4d01
Aydın Erol <>
2023-11-06 16:35:31 +0300

    Set additional constraints for tableViewCell class

--- TravioApp/TravioApp/TravioApp/Views/DropCell.swift
+++ TravioApp/TravioApp/TravioApp/Views/DropCell.swift
@@ -12,18 +12,19 @@ import SnapKit
 
 class DropCell: UITableViewCell {
     
+    static let reuseIdentifier = "ExpandableCell"
+    
     //MARK: -- Properties
     
-    var isExpanded:Bool = false {
-        didSet{
-        }
-        
-    }
+//    var isExpanded:Bool = false
     
     var dropCellViewModel:DropCellViewModel? {
         didSet{
             lblHeader.text = dropCellViewModel?.title
             lblDescription.text = dropCellViewModel?.description
+            lblDescription.isHidden = !dropCellViewModel!.isExpanded
+            print("\(!dropCellViewModel!.isExpanded)")
+            imgDropButton.image = (dropCellViewModel!.isExpanded ? UIImage(systemName: "chevron.up") : UIImage(systemName: "chevron.down"))?.withRenderingMode(.alwaysOriginal)
         }
     }
     
@@ -34,17 +35,18 @@ class DropCell: UITableViewCell {
         lbl.text = "placeHolderTitle"
         lbl.font = UIFont(name: "Poppins-Medium", size: 14)
         lbl.textColor = UIColor(named: "textColor")
-        lbl.numberOfLines = 2
+        lbl.numberOfLines = 0
         return lbl
     }()
     
     private lazy var lblDescription: UILabel = {
         let lbl = UILabel()
-        lbl.text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        lbl.text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
         lbl.font = UIFont(name: "Poppins-Regular", size: 10)
         lbl.textColor = UIColor(named: "textColor")
-        lbl.isHidden = true
-        lbl.numberOfLines = 5
+//        lbl.isHidden = false
+        lbl.numberOfLines = 0
+        lbl.sizeToFit()
         return lbl
     }()
     
@@ -56,16 +58,20 @@ class DropCell: UITableViewCell {
         view.clipsToBounds = true
         view.layer.cornerRadius = 16
         view.layer.borderColor = UIColor.black.cgColor
-        view.layer.shadowRadius = 16
-        view.layer.shadowOpacity = 0.15
+        view.layer.shadowRadius = 20
+        view.layer.shadowOpacity = 0.45
         view.layer.maskedCorners = [.layerMinXMinYCorner]
         return view
     }()
+    private lazy var viewSpace:UIView = {
+        let v = UIView()
+        return v
+    }()
     
     private lazy var imgDropButton:UIImageView = {
-        let imgView = UIImageView()
-        let img = UIImage(systemName: "chevron.down")?.withRenderingMode(.alwaysOriginal)
-        imgView.image = img
+        let imgView = UIImageView(image: UIImage(systemName: "chevron.down")?.withRenderingMode(.alwaysOriginal))
+        //let img = UIImage(systemName: "chevron.down")?.withRenderingMode(.alwaysOriginal)
+        //imgView.image = img
         
         return imgView
     }()
@@ -90,8 +96,7 @@ class DropCell: UITableViewCell {
     
     private func setupViews() {
         // Add here the setup for the UI
-        self.backgroundColor = UIColor(named: "viewBackgroundColor")
-        self.backgroundColor = .systemGray3
+        self.selectionStyle = .none
         self.addSubviews(dropView)
         dropView.addSubviews(lblHeader, imgDropButton, lblDescription)
         
@@ -101,33 +106,40 @@ class DropCell: UITableViewCell {
     private func setupLayout() {
         // Add here the setup for layout
         dropView.snp.makeConstraints({ dv in
-            dv.leading.equalToSuperview()//.offset(24)
-            dv.trailing.equalToSuperview()//.offset(-24)
-            dv.height.equalTo(setHeightClosure())
-            dv.center.equalToSuperview()
+            dv.top.equalToSuperview()
+            dv.bottom.equalToSuperview()
+            dv.leading.equalToSuperview()
+            dv.trailing.equalToSuperview()
             
         })
         
         lblHeader.snp.makeConstraints({lbl in
-            
-            lbl.centerY.equalToSuperview()
+            lbl.top.equalToSuperview().offset(16)
+            lbl.bottom.equalTo(lblDescription.snp.top).offset(-12)
             lbl.leading.equalToSuperview().offset(12)
             lbl.trailing.equalToSuperview().offset(-12*4)
             
         })
         
         lblDescription.snp.makeConstraints({ lbl in
-            lbl.top.equalTo(lblHeader.snp.bottom).offset(12)
+            lbl.top.equalTo(lblHeader.snp.bottom)
+            lbl.bottom.equalToSuperview()
             lbl.leading.equalTo(lblHeader)
             lbl.trailing.equalToSuperview().offset(-15)
             
         })
         
         imgDropButton.snp.makeConstraints({ img in
+            img.top.equalTo(lblHeader.snp.bottom)
+            img.bottom.equalTo(lblHeader.snp.bottom).offset(-lblHeader.frame.height/1.5)
             img.trailing.equalToSuperview().offset(-18.37)
-            img.centerY.equalToSuperview()
             
         })
+        
+//        viewSpace.snp.makeConstraints({ view in
+//            view.top.equalTo(dropView.snp.bottom)
+//
+//        })
     }
 }
 
