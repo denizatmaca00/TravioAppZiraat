80929da2fd4b8c0c7ffcd4204b6ff1ae97f35faf
Deniz <>
2023-11-06 12:23:57 +0300

    added TermsOfUseVC, AboutUs, EditProfileVC

---
+++ TravioApp/TravioApp/TravioApp/Views/TermsOfUseVC.swift
@@ -0,0 +1,29 @@
+//
+//  TermsOfUseVC.swift
+//  TravioApp
+//
+//  Created by web3406 on 11/6/23.
+//
+
+import UIKit
+
+class TermsOfUseVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
