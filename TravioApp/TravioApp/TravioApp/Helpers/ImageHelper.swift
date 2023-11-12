ecc75546dbbb889e0795100237286bb515f8c380
Aydın Erol <>
2023-11-12 21:21:03 +0300

    Add activity indicator and loading animation to image fetcher. Separate image loading methods into two: setImage and retrieveImage

--- TravioApp/TravioApp/TravioApp/Helpers/ImageHelper.swift
+++ TravioApp/TravioApp/TravioApp/Helpers/ImageHelper.swift
@@ -11,23 +11,63 @@ import UIKit
 
 class ImageHelper{
     
-}
-
-extension ImageHelper {
+    // MARK: Properties
+    
+    let imgPlaceholder:UIImage = (UIImage(systemName: "camera.metering.none")?.withRenderingMode(.alwaysOriginal).withTintColor(.systemGray3))!
+    
+    // Processors
+    //private let imgProcessor: KingfisherOptionsInfoItem = .processor(RoundCornerImageProcessor(cornerRadius: 16)) // round image corners
+    //private let blurProcessor: KingfisherOptionsInfoItem = .processor(BlurImageProcessor(blurRadius: 4)) // blur image
+    
+    private let imgTransition: KingfisherOptionsInfoItem = .transition(.fade(0.5)) // image loading animation
+    
+    
+    // MARK: KF-SetImage
+    
     func setImage(imageURL:URL, imageView:UIImageView){
-        KingfisherManager.shared.retrieveImage(with: imageURL, options: nil, progressBlock: nil) { result in
+        let imgDownsampler: KingfisherOptionsInfoItem = .processor(DownsamplingImageProcessor(size: imageView.bounds.size))
+        let kfOptions: KingfisherOptionsInfo = [imgDownsampler, imgTransition, .cacheOriginalImage]
+        
+        imageView.kf.indicatorType = .activity
+
+        imageView.kf.setImage(with: imageURL, placeholder: imgPlaceholder, options: kfOptions) { result in
+            switch result{
+            case .success(_):
+                //print(value.cacheType)
+                //print(value.source)
+                return
+                //imageView.image = value.image
+            case .failure(_): //let error case
+                //print("image not found on: \(imageURL)")
+                //imageView.image = imgPlaceholder
+                return
+            }
+        }
+    }
+    
+    
+    // MARK: KF-RetrieveImage
+    
+    func setImageCustom(imageURL:URL, imageView:UIImageView){
+        
+        let kfOptions:KingfisherOptionsInfo = [imgTransition]
+        
+        imageView.kf.indicatorType = .activity
+        
+        KingfisherManager.shared.retrieveImage(with: imageURL, options: kfOptions, progressBlock: nil) { result in
             
             switch result{
             case .success(let value):
                 imageView.image = value.image
             case .failure(_): //let error case
-                print("image not found on: \(imageURL)")
-                // null image placeholder:
-                imageView.image = UIImage(systemName: "camera.metering.none")?
-                    .withRenderingMode(.alwaysOriginal)
-                    .withTintColor(.systemGray3)
+                //print("image not found on: \(imageURL)")
+                imageView.image = self.imgPlaceholder
                 return
             }
         }
     }
+}
+
+extension ImageHelper{
+    
 }
