ee241ba1e3ef28a034433306633844fe4b843581
Aydın Erol <>
2023-11-13 15:01:59 +0300

    ActivityIndicator is now a UIViewController extension

--- TravioApp/TravioApp/TravioApp/UIElements/AppActivityIndicator.swift
+++ TravioApp/TravioApp/TravioApp/UIElements/AppActivityIndicator.swift
@@ -9,26 +9,10 @@ import UIKit
 
 class AppActivityIndicator: UIView {
     
-    var targetVC:LoginVC?
-    
-//    override init(frame: CGRect) {
-//        super.init(frame: frame)
-//    }
-    
-//    init(frame:CGRect, targetVC: UIViewController? = nil) {
-//        super.init(frame: frame)
-//        self.targetVC = targetVC
-//    }
-//    
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-    
     lazy var activityIndicator:UIActivityIndicatorView = {
         let ai = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 20 , height: 20))
         ai.color = UIColor(named: "backgroundColor")
         ai.style = .large
-        ai.center = (targetVC?.view.center)! //(CGPoint(x: targetVC.frame.width, y:self.frame.height))
         ai.hidesWhenStopped = true
         return ai
     }()
@@ -36,38 +20,30 @@ class AppActivityIndicator: UIView {
     lazy var viewBlur:UIVisualEffectView = {
         let blurEffect = UIBlurEffect(style: .systemUltraThinMaterialLight)
         let blurEffectView = UIVisualEffectView(effect: blurEffect)
-        blurEffectView.frame = (targetVC?.view.frame)! //?? CGRect(x: 0, y: 0, width: 200, height: 400)
         blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         return blurEffectView
     }()
-    
-    func getEffect()->UIView{
-        addSubviews(viewBlur)
-        viewBlur.contentView.addSubviews(activityIndicator)
-        
-        return viewBlur
-    }
-    
-    //@discardableResult
-    func getLoadView(){
-        let isLoading = (targetVC?.viewModel.isLoading)!
-        if isLoading{
-            targetVC?.view.addSubviews(viewBlur)
-            viewBlur.contentView.addSubviews(activityIndicator)
-            self.activityIndicator.startAnimating()
-        }else{
-            viewBlur.removeFromSuperview()
-            self.activityIndicator.removeFromSuperview()
-        }
-    }
 }
 
 extension UIViewController {
+    
     func showIndicator(){
+        let viewBlur = AppActivityIndicator().viewBlur
+        viewBlur.frame = self.view.frame
         
+        let indicatorLoading = AppActivityIndicator().activityIndicator
+        indicatorLoading.center = self.view.center
+        
+        self.view.addSubviews(viewBlur)
+        viewBlur.contentView.addSubview(indicatorLoading)
+        
+        indicatorLoading.startAnimating()
     }
     
-    func removeIndicator(){
-        
+    func hideIndicator(){
+        let viewBlur = AppActivityIndicator().viewBlur
+        let indicatorLoading = AppActivityIndicator().activityIndicator
+        viewBlur.removeFromSuperview()
+        indicatorLoading.removeFromSuperview()
     }
 }
