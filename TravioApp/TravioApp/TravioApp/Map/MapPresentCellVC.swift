d2d202713f3d0f2f4f0382d62d8e87fe1fbbc8c1
Aydın Erol <>
2023-11-17 16:18:00 +0300

    Add imagePickerController

--- TravioApp/TravioApp/TravioApp/Map/MapPresentCellVC.swift
+++ TravioApp/TravioApp/TravioApp/Map/MapPresentCellVC.swift
@@ -7,25 +7,21 @@
 
 import UIKit
 
-class MapPresentCellVC: UICollectionViewCell {
-
-
-    var viewModel = MapVM()
+class MapPresentCellVC: UICollectionViewCell, UINavigationControllerDelegate {
     
-    var visitCellViewModel: VisitCellViewModel? {
-        didSet {
-            
-        }
-    }
     
-    private lazy var cellView: UIView = {
-        let view = UIView()
-        view.isUserInteractionEnabled = true
+    var viewModel = MapPresentVM()
+    
+    var presentClosure:((UIImagePickerController)->Void)?
+    var dismissClosure:(()->Void)?
+    
+    private lazy var cellView: UIImageView = {
+        let view = UIImageView()
         view.backgroundColor = .white
         view.layer.cornerRadius = 16
         return view
     }()
- 
+    
     private lazy var addPhotoIcon: UIImageView = {
         let img = UIImageView()
         img.image = UIImage(named: "addPhoto")
@@ -38,8 +34,6 @@ class MapPresentCellVC: UICollectionViewCell {
         btn.setTitle("Add Photo", for: .normal)
         btn.setTitleColor(UIColor(named: "inactiveButtonColor"), for: .normal)
         btn.titleLabel?.font = .Fonts.textFieldText.font
-//        btn.addTarget(self, action: #selector(addPhotoButtonTapped), for: .touchUpInside)
-        
         return btn
     }()
     
@@ -52,7 +46,14 @@ class MapPresentCellVC: UICollectionViewCell {
         super.init(coder: coder)
         setupViews()
     }
-
+    
+    func imagePicker (){
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        presentClosure!(picker)
+    }
+    
     func setupViews() {
         
         self.addSubviews(cellView)
@@ -62,21 +63,46 @@ class MapPresentCellVC: UICollectionViewCell {
     }
     
     func setupLayout() {
-
+        
         cellView.snp.makeConstraints { make in
             make.height.equalTo(180)
             make.leading.equalToSuperview().offset(24)
             make.width.equalTo(270)
         }
+        
         addPhotoIcon.snp.makeConstraints { make in
             make.top.equalTo(cellView).offset(79)
             make.height.equalTo(35)
             make.centerX.equalTo(cellView)
         }
+        
         addPhotoBtn.snp.makeConstraints({lbl in
             lbl.top.equalTo(addPhotoIcon.snp.bottom)
             lbl.centerX.equalTo(cellView)
         })
+    }
+}
+
+extension MapPresentCellVC:UIImagePickerControllerDelegate{
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[.originalImage] as? UIImage{
+            cellView.image = selectedImage
+            viewModel.imageData.append(selectedImage)
+            dismissClosure!()
+        }
+    }
+    
+    func handlePickedImage(_ image: UIImage) {
+        // Seçilen fotoğrafı işleyecek
+        // imageview içine koysan daha iyi olaiblir
+        
+        // Daha sonra bu fotoğrafı API'ye yükle
+        //  uploadPhoto(image: image)
     }
 }
 
@@ -86,7 +112,8 @@ import SwiftUI
 @available(iOS 13, *)
 struct MapPresentCellVC_Preview: PreviewProvider {
     static var previews: some View{
-
+        
+        MapPresentVC().showPreview().ignoresSafeArea(.all)
         MapPresentCellVC().showPreview()
     }
 }
