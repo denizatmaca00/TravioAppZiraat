a13a201f62d7adfb54e743eb6c447d795a79e741
Aydın Erol <>
2023-11-18 16:30:53 +0300

    Add methods to manage imagePicker and it's data inside MapPresentVM Add functions to upload image and post place, and security checks for these functions Indentation correction Add documentation and comments

--- TravioApp/TravioApp/TravioApp/Map/MapPresentVM.swift
+++ TravioApp/TravioApp/TravioApp/Map/MapPresentVM.swift
@@ -11,56 +11,117 @@ import UIKit
 class MapPresentVM{
     
     var placeInfo: AddPlace = AddPlace(place: "", title: "", description: "", cover_image_url: "", latitude: 0, longitude: 0)
-
-    var imageData:[UIImage] = []
+    
+    var picker: UIImagePickerController?
+    var lastImage:UIImage?
+    
     var imageURL:[String] = []{
         didSet{
-            placeInfo.cover_image_url = imageURL.first!
+            self.placeInfo.cover_image_url = self.imageURL.first!
+            self.initPostPlace()
         }
     }
     
+    var imageData:[UIImage] = []
+    
+    /// Used to present and dismiss acitivity indicator during wait for server data
     var isLoading:Bool = false {
         didSet{
-            self.updateLoadingStatus?()
+            self.updateLoadingStatus?(self.isLoading)
         }
     }
     
     // Closures
-    var presentClosure:((UIImagePickerController)->Void)?
-    var dismissClosure:(()->Void)?
-   // var showAlertClosure: ((String, String) -> Void)?
-    var updateLoadingStatus: ( ()->() )?
+    // var showAlertClosure: ((String, String) -> Void)?
+    var reloadCollectionViewClosure: (()->())?
+    var updateLoadingStatus: ( (Bool)->() )?
+    var updateMapClosure: ( ()->Void )?
+    var dismissClosure: (()->())?
     
-    func uploadPhoto(photo: UIImage) {
-        let params = ["pp_url": photo]
+    func fetchData(in cell:MapPresentCellVC, with indexPath:IndexPath){
+        cell.fillCellWith(image: lastImage!)
+    }
+    
+    /// Handles processes required to save a place
+    /// Uploads photos stored inside imageData
+    /// Using
+    func savePlace(){
+        
+        /// initial check for non-empty image data
+        if imageData.count > 0 {
+                /// upload photo
+                for photo in [imageData[0]]{
+                    uploadPhoto(photo: photo)
+                }
+        }else{
+            print("No images found")
+        }
+    }
+    
+    private func uploadPhoto(photo: UIImage) {
+        self.isLoading = true
+        
+        let params = ["image_url": photo]
         
         NetworkingHelper.shared.uploadPhoto(images: imageData, urlRequest: .uploadAddPhoto(params: params), callback: { (result: Result<AddPhotoUploadMultipartMessages, Error>) in
             switch result {
             case .success(let success):
                 self.imageURL = success.urls
-                
+                print("URL:: \(success.urls)")
             case .failure(let failure):
                 print("ERROR: \(failure)")
+            }
+            self.isLoading = false
+        })
+    }
+    
+    private func initPostPlace(){
+        postAddNewPlace(placeInfo: self.placeInfo, completion: { [weak self] result in
+            switch result {
+            case .success(let response):
+                if let messages = response.message {
+                    self!.dismissClosure?()
+                    self!.updateMapClosure?()
+                }
+            case .failure(let error):
+                print("Error: \(error)")
             }
         })
     }
     
     /// post place to API
-    func postAddNewPlace(place: String?, title: String, description: String, cover_image_url: String,  latitude: Double, longitude: Double,completion: @escaping (Result<Messages, Error>) -> Void) {
+    private func postAddNewPlace(placeInfo:AddPlace ,completion: @escaping (Result<Messages, Error>) -> Void) {
         self.isLoading = true
         
-        let paramsPost = ["place": place!, "title": title, "description": description, "cover_image_url": cover_image_url, "latitude" : latitude, "longitude": longitude] as [String : Any]
-
+        let paramsPost = ["place": placeInfo.place, "title": placeInfo.title, "description": placeInfo.description, "cover_image_url": placeInfo.cover_image_url, "latitude" : placeInfo.latitude, "longitude": placeInfo.longitude] as [String : Any]
+        
         NetworkingHelper.shared.dataFromRemote(urlRequest: .postAddPlace(params: paramsPost)) { (result: Result<Messages, Error>) in
             
             switch result {
-                case .success(let response):
-                    completion(.success(response))
+            case .success(let response):
+                completion(.success(response))
                 
-                case .failure(let error):
-                    completion(.failure(error))
+            case .failure(let error):
+                completion(.failure(error))
             }
             self.isLoading = false
         }
     }
+//    private func updatePlace(placeInfo:AddPlace ,completion: @escaping (Result<Messages, Error>) -> Void) {
+//        self.isLoading = true
+//        
+//        let paramsPost = ["place": placeInfo.place, "title": placeInfo.title, "description": placeInfo.description, "cover_image_url": placeInfo.cover_image_url, "latitude" : placeInfo.latitude, "longitude": placeInfo.longitude] as [String : Any]
+//        
+//        NetworkingHelper.shared.dataFromRemote(urlRequest: .postAddPlace(params: paramsPost)) { (result: Result<Messages, Error>) in
+//            
+//            switch result {
+//            case .success(let response):
+//                completion(.success(response))
+//                
+//            case .failure(let error):
+//                completion(.failure(error))
+//            }
+//            self.isLoading = false
+//        }
+//    }
 }
