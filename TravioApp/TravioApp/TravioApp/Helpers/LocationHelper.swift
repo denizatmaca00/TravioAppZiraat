4919cac429edc28192c8b078a7576699a63ad7d0
Aydın Erol <>
2023-11-21 15:30:54 +0300

    Update locationHelper to fill PresentVC location textfield.

--- TravioApp/TravioApp/TravioApp/Helpers/LocationHelper.swift
+++ TravioApp/TravioApp/TravioApp/Helpers/LocationHelper.swift
@@ -12,15 +12,12 @@ class LocationHelper{
     
 }
 
-//unused backup helper method to get closest district/city name for given latitude:longitude coordinates
-extension VisitsVM {
+// Helper method to get closest district/city name for given latitude:longitude coordinates
+extension MapPresentVC {
     // This function can be used to create cellView with city data obtained by Lat:Long data in favorites array
-    private func setCellViewByLatLong(favorite:Place){
-        var cityArr:[String] = []
-        var viewModels = [VisitCellViewModel]()
+    func getLocalName(latitude:Double, longitude:Double){
         
-        let location = CLLocation(latitude: favorite.latitude, longitude: favorite.longitude)
-        var city:String = "nilDefault"
+        let location = CLLocation(latitude: latitude, longitude: longitude)
         
         CLGeocoder().reverseGeocodeLocation(location) { placemarks , error in
             
@@ -28,16 +25,12 @@ extension VisitsVM {
                 guard let placemark = placemarks?.last else {
                     return
                 }
-                city = placemark.locality ?? " "
-                cityArr.append(city)
-                let cvm = VisitCellViewModel(image: URL(string: favorite.cover_image_url)!,
-                                             placeName: favorite.title,
-                                             city: city)
-                viewModels.append(cvm)
-                self.cellViewModels = viewModels
-            }
-            if city != "nilDefault"{
-                cityArr.append(city)
+                let city = placemark.locality ?? " "
+                if let country = placemark.country {
+                    self.localName = "\(country), \(city)"
+                } else{
+                    self.localName = "\(city)"
+                }
             }
         }
     }
