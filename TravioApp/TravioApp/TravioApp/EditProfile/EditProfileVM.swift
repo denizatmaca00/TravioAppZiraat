77e9cdce13089d6631683fc624ac32c78f492893
Aydın Erol <>
2023-11-24 23:41:47 +0300

    Code refactoring on showAlertClosure, imageURL and selectedImage Move alerts into VM class Code cleaning and documentation

--- TravioApp/TravioApp/TravioApp/EditProfile/EditProfileVM.swift
+++ TravioApp/TravioApp/TravioApp/EditProfile/EditProfileVM.swift
@@ -12,66 +12,84 @@ import UIKit
 
 class EditProfileVM {
     
-    var editProfile: EditProfile = EditProfile(full_name: "", email: "", pp_url: "")
-    var profileUpdateClosure: (() -> Void)?
+    // MARK: Properties
     
+    // Struct-Related
+    var editProfile: EditProfile = EditProfile(full_name: "", email: "", pp_url: "")
+    var profile: Profile? {
+        didSet {
+            profileUpdateClosure?()
+        }
+    }
+    
+    // Flags
     var isLoading:Bool = false {
         didSet{
             indicatorUpdateClosure?(self.isLoading)
         }
     }
-    var indicatorUpdateClosure:((Bool)->(Void))?
-        
-    var profile: Profile? {
-            didSet {
-                profileUpdateClosure?()
-            }
-        }
-
-    func profileUpdated() {
-        profileUpdateClosure?()
-    }
-    var showAlertClosure: ((String, String) -> Void)?
+    var doUploadWork:Bool = false
     
+    // Closures
+    var profileUpdateClosure: (() -> Void)?
+    var indicatorUpdateClosure:((Bool)->(Void))?
+    var showAlertClosure: ((String, String) -> Void)?
     var reloadEditProfileClosure: (() -> ())?
-    var imagesDatas:[UIImage] = []
-    var imageURL:[String] = []{
+    
+    // Struct and UI Properties
+    var selectedImage:UIImage?
+    var imageURL:String? {
         didSet{
-            editProfile.pp_url = imageURL.first!
+            editProfile.pp_url = imageURL!
             putEditProfileInfos()
         }
     }
-
-    func editProfilePhotoUpload(photo: UIImage){
-        self.isLoading = true
-
-        let params = ["pp_url": photo]
-
-        NetworkingHelper.shared.uploadPhoto(images: imagesDatas, urlRequest: .uploadAddPhoto(params: params), callback: { (result: Result<AddPhotoUploadMultipartMessages, Error>) in
-            switch result {
-            case .success(let success):
-                self.imageURL = success.urls
-
-            case .failure(let failure):
-                print("Başarısız yanıt: \(failure)")
-            }
-            self.isLoading = false
-
-        })
-        
+    
+    // MARK: Public Functions
+    
+    func profileUpdated() {
+        profileUpdateClosure?()
     }
     
-    func putEditProfileInfos(){
-
+    func postData(completion: (()->()) ) -> Void {
+        
+        if doUploadWork {
+            editProfilePhotoUpload()
+        }
+        
+        putEditProfileInfos()
+    }
+    
+    // MARK: Private Functions
+    private func editProfilePhotoUpload() -> Void {
+        
+        self.isLoading = true
+        
+        let params = ["pp_url": selectedImage]
+        NetworkingHelper.shared.uploadPhoto(images: [selectedImage!], urlRequest: .uploadAddPhoto(params: params), callback: { (result: Result<AddPhotoUploadMultipartMessages, Error>) in
+            switch result {
+            case .success(let success):
+                self.imageURL = success.urls.first
+                
+            case .failure(_):
+                self.doUploadWork = true
+            }
+            self.isLoading = false
+        })
+    }
+    
+    private func putEditProfileInfos() -> Void {
+        
+        self.isLoading = true
         let params = ["full_name": editProfile.full_name, "email": editProfile.email, "pp_url": editProfile.pp_url]
         NetworkingHelper.shared.dataFromRemote(urlRequest: .putEditProfile(params: params), callback: {  (result: Result<Messages, Error>) in
             switch result {
-            case .success(let success):
-                print("Başarılı yanıt: \(success)")
-            case .failure(let failure):
-                print("Başarısız yanıt: \(failure)")
+            case .success(_):
+                self.showAlertClosure?("Notification", "Updated profile successfully.")
+            case .failure(_):
+                self.showAlertClosure?("Error", "Failed to update profile.")
             }
-
+            self.isLoading = false
         })
     }
 }
