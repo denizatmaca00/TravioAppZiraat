2f7be71d5acc839b5cb553fa21343d01b965a42a
Ece Poyraz <ecepoyraz6694@gmail.com>
2023-11-28 15:03:03 +0300

    code refactor

--- TravioApp/TravioApp/TravioApp/SecuritySettings/SecuritySettingsVM.swift
+++ TravioApp/TravioApp/TravioApp/SecuritySettings/SecuritySettingsVM.swift
@@ -13,8 +13,8 @@ class SecuritySettingsVM{
     var passwordChangeAlertClosure: ((String,String)->Void)?
    
     func putPassword(password:Password){
-        let pass = ["new_password" : password.new_password]
-        NetworkingHelper.shared.dataFromRemote(urlRequest: .putPassword(params: pass), callback:{(result:Result<Messages,Error>) in
+        let passwordNew = ["new_password" : password.new_password]
+        NetworkingHelper.shared.dataFromRemote(urlRequest: .putPassword(params: passwordNew), callback:{(result:Result<Messages,Error>) in
             switch result{
             case .success(let params):
                 print(params)
