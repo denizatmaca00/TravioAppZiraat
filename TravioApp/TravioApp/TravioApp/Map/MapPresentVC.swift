293ca56ccbd9367d6b6857dbe3ceb6d1b370ae30
Aydın Erol <>
2023-11-28 12:15:16 +0300

    Move function that recommend location into MapPresentVC and remove old file.

--- TravioApp/TravioApp/TravioApp/Map/MapPresentVC.swift
+++ TravioApp/TravioApp/TravioApp/Map/MapPresentVC.swift
@@ -1,4 +1,3 @@
-
 //
 //  MapPresentVC.swift
 //  TravioApp
@@ -7,6 +6,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 class MapPresentVC: UIViewController, UINavigationControllerDelegate, UITextViewDelegate {
     
@@ -300,6 +300,29 @@ extension MapPresentVC: UIImagePickerControllerDelegate{
     }
 }
 
+// Helper method to get closest district/city name for given latitude:longitude coordinates
+extension MapPresentVC {
+    // This function can be used to create cellView with city data obtained by Lat:Long data in favorites array
+    func getLocalName(latitude:Double, longitude:Double){
+        
+        let location = CLLocation(latitude: latitude, longitude: longitude)
+        
+        CLGeocoder().reverseGeocodeLocation(location) { placemarks , error in
+            
+            if error == nil && placemarks!.count > 0 {
+                guard let placemark = placemarks?.last else {
+                    return
+                }
+                let city = placemark.locality ?? " "
+                if let country = placemark.country {
+                    self.localName = "\(country), \(city)"
+                } else{
+                    self.localName = "\(city)"
+                }
+            }
+        }
+    }
+}
 
 
 #if DEBUG
